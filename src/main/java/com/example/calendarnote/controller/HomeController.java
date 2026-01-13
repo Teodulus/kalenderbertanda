1f6388d2e2d9745784bcd4455f7a5ea57804fd76
Teodulus <160575234+Teodulus@users.noreply.github.com>
2026-01-13 10:19:46 +0700

    htmlll

--- src/main/java/com/example/calendarnote/controller/HomeController.java
+++ src/main/java/com/example/calendarnote/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    // Fungsi: Kalau ada yang buka link utama "/", langsung lempar ke index.html
+    // Fungsi: Kalau ada yang buka link utama "/", langsung lempar ke inndex.html
     @GetMapping("/")
     public String home() {
         return "redirect:/index.html";
